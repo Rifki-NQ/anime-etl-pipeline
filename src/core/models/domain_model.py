8f36faefca7e90fcd4385b23e1fb813676a18147
Rifki-NQ <rifqinf950@gmail.com>
2026-05-27 16:47:03 +0700

    feat(model): add AnimeDataModel as domain model

---
+++ src/core/models/domain_model.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+@dataclass
+class AnimeDataModel:
+    id: int
+    id_mal: int | None
+    romaji_title: str
+    english_title: str | None
+    native_title: str | None
+    preferred_title: str
+    type: str
+    format: str
+    description: str | None
+    start_date: str | None
+    end_date: str | None
+    season: str | None
+    season_year: int | None
+    episodes: int | None
+    duration: int | None
+    country_of_origin: str
+    source: str | None
+    hashtag: str | None
+    updated_at: int
+    genres: str | None
+    synonyms: list[str]
+    average_score: int | None
+    mean_score: int | None
+    popularity: int
+    trending: int
+    favourites: int
+    animation_studio: str | None
